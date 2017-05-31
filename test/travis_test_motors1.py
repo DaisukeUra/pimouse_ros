7a0e44eec4d8684e18a16ef0fb56c78d435d6fbb
DaisukeURA <daisukeura1230@gmail.com>
2017-05-31 07:57:49 +0000

    mod: fix testfile

--- test/travis_test_motors1.py
+++ test/travis_test_motors1.py
@@ -29,19 +29,19 @@ class MotorTest(unittest.TestCase):
         self.file_check("rtmotor_raw_l0", m.left_hz, "wrong left motor_raw")
         self.file_check("rtmotor_raw_r0", m.right_hz, "wrong right motor_raw")
 
-        def test_put_cmd_vel(self):
-            pub = rospy.Publisher('cmd_vel', Twist)
-            m = Twist()
-            m.linear.x = 0.1414
-            m.angular.z  = 1.57
+    def test_put_cmd_vel(self):
+        pub = rospy.Publisher('cmd_vel', Twist)
+        m = Twist()
+        m.linear.x = 0.1414
+        m.angular.z  = 1.57
 
-            for i in range(10):
-                pub.publish(m)
-                time.sleep(0.1)
+        for i in range(10):
+            pub.publish(m)
+            time.sleep(0.1)
 
-            time.sleep(1.1)
-            self.file_check("rtmotor_raw_l0", 0, "dont stop left motor")
-            self.file_check("rtmotor_raw_r0", 0, "dont stop right motor")
+        time.sleep(1.1)
+        self.file_check("rtmotor_raw_l0", 0, "dont stop left motor")
+        self.file_check("rtmotor_raw_r0", 0, "dont stop right motor")
             
 if __name__ == "__main__":
     time.sleep(3)
